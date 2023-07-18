1f52608ae9c71543431980cac7d6bc2b53264ac7
mkerekes1427 <mkerekes53@gmail.com>
2023-07-18 16:54:35 -0400

    Refactored. stats_calculations file

--- calculations/views.py
+++ calculations/views.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from scipy.stats import ttest_1samp
+from .stats_calculations import calc_ttest1
 
 from django.shortcuts import render
 from django.contrib import messages
@@ -36,30 +36,7 @@ def one_sample(request):
 
         if test == "t":
 
-            col1 = df.iloc[:, 0]
-
-            t_test = ttest_1samp(col1, value, alternative=alternative)
-
-            t_stat = t_test[0]
-            p_value = t_test[1]
-            freedom = t_test.df
-
-
-            interval = t_test.confidence_interval(1-alpha)
-
-            low = interval[0]
-            high = interval[1]
-
-            context = {
-                "calculations" : True,
-                "t_stat" : round(t_stat, 4),
-                "p_value" : round(p_value, 4),
-                "freedom" : freedom,
-                "confidence" : (1-alpha)*100,
-                "low" : round(low, 4),
-                "high" : round(high, 4),
-                "alpha" : alpha
-            }
+            context = calc_ttest1(df, value, alternative, alpha)
 
             return render(request, "one-sample.html", context=context)
 
